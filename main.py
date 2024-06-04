9d35e8bebb9b6053e0d8fa9a6e473dcc8a8cef68
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-04 23:18:49 +0300

    create registration for user

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import dotenv_values
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash
+
+from models import db, User
 
 # берем переменные окружения из файла
 env = dotenv_values(dotenv_path='.env')
@@ -11,10 +14,11 @@ DB_URI = f'postgresql+psycopg2://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"
 # Инициализация приложения
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-# app.config['SECRET_KEY'] = env['FLASK_SECRET_KEY']
-# app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = env['FLASK_SECRET_KEY']
+app.config['WTF_CSRF_ENABLED'] = True
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.cli.command("create-db")
@@ -26,13 +30,34 @@ def create_db():
 # Декоратор, указывающий, что функция hello() является callback для корневого URL.
 @app.route("/")
 def index():
-    # return render_template('main_page.html')
     return render_template('main_page.html')
 
 
-@app.route("/registration")
+@app.route("/registration", methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            flash('Пароли не совпадают!')
+            return redirect(url_for('registration'))
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Вы успешно зарегистрировались! Теперь можете войти в личный кабинет')
+        return redirect(url_for('login'))  # Предполагаем, что у вас есть маршрут для логина
+
     return render_template('registration.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
--- models/user.py
+++ models/user.py
@@ -9,6 +9,6 @@ class User(db.Model):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    login: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
     date_of_registration: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
