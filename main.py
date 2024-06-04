ba2d8d5adcf5313bb1154a32c1a4c81ba499cd6a
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-04 15:03:36 +0300

    create flask app with main_page

---
+++ main.py
@@ -0,0 +1,28 @@
+from dotenv import dotenv_values
+from flask import Flask, render_template
+from models import db
+
+# берем переменные окружения из файла
+env = dotenv_values(dotenv_path='.env')
+# print(env)
+
+DB_URI = f'postgresql+psycopg2://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@{env["DB_HOST"]}:{env["DB_PORT"]}/{env["DB_NAME"]}'
+# DB_URI = f'postgresql+psycopg2://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@pg:5432/shop'
+
+# Инициализация приложения
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+# app.config['SECRET_KEY'] = env['FLASK_SECRET_KEY']
+# app.config['WTF_CSRF_ENABLED'] = True
+
+db.init_app(app)
+
+
+# Декоратор, указывающий, что функция hello() является callback для корневого URL.
+@app.route("/")
+def index():
+    return render_template('main_page.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
