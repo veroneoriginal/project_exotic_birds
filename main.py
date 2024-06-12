3ecf685886de5be5dd043a2626a9ce2ac2a7bebf
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-12 23:32:37 +0300

    added filtering by tags and users

--- main.py
+++ main.py
@@ -280,8 +280,20 @@ def edit_post(post_id):
 
 @app.route('/blog')
 def blog():
-    posts = Post.query.filter_by(is_published=True).all()
+    # Извлечение значения параметра tag/user из строки запроса URL (если он существует).
+    tag_id = request.args.get('tag')
+    user_id = request.args.get('user')
+
+    if tag_id:
+        posts = Post.query.filter(Post.tags.any(id=tag_id), Post.is_published).all()
+    elif user_id:
+        posts = Post.query.filter_by(user_id=user_id, is_published=True).all()
+    else:
+        posts = Post.query.filter_by(is_published=True).all()
+
     return render_template('blog.html', posts=posts)
+
+
 
 
 if __name__ == "__main__":
